811146998590c26187cdb477cf6b50ebd5fd5549
JeremyE96 <elliottjeremy26@gmail.com>
2020-07-14 16:53:55 +0100

    Connected to mySQL DB, able to print uniNames

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-//Module imports
+// Module imports
 const express = require('express');
 
-//Local imports
-const homeRoute = require('./controller/home');
+// Local imports
+const homeRoute = require('./controller/home.js');
+const db = require('./util/database.js')
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use('/', homeRoute.getHomePage )
+db.query('SELECT universityName FROM leaguetableranking')
+  .then(([rowData, columnData]) => {
+    let array = [];
+    rowData.forEach(rowValue => {
+      array.push(rowValue.universityName);
+    });
+    console.log(array);
+
+  })
+  .catch((err) => console.log(err));
+
+app.use('/', homeRoute.getHomePage)
 
 app.listen(3000);
