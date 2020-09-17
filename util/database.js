253b4a5b29052f6374fe8e1f48b6e2613ec253ab
JeremyE96 <elliottjeremy26@gmail.com>
2020-09-17 08:56:51 +0100

    .

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   database: 'guardianleaguetable',
-  password: 'Sponge123!'
+  password: 'Manado1969!'
 })
 
 const connection = pool.promise();
